61bd004f576290da1794888e53210b0b96d97c10
James Lenahan <james.k.lenahan@gmail.com>
2019-12-04 18:18:38 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,3 +1,16 @@
 ## code your solution here. 
 class Cat 
   def name=(cats_name)
+    @name = cats_name
+  end
+  
+  def name
+    @name
+  end
+  def meow=(meow)
+    @meow = meow
+  end
+  def meow
+    @meow
+  end
+end
